f63b383b2b6d6823d474982639324f5ef3ad2e97
Nico-Adamo <nicktwoa@gmail.com>
2021-06-25 13:07:46 -0700

    Initial commit

---
+++ experiments/train_baseline_resnet.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+python3 train.py --max-epoch 300 \
+                 --save-epoch 20 \
+                 --shot 5 \
+                 --train-way 10 \
+                 --test-way 5 \
+                 --query 15 \
+                 --save-path models/proto-1 \
+                 --gpu 0 \
+                 --model ResNet12 \
+                 --project CNNF-Prototype
+
+wait
+echo "All done"
