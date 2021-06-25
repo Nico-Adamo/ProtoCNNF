af54fe98eac976d58daca638f09b70a6d136e2c1
Nico-Adamo <nicktwoa@gmail.com>
2021-06-25 15:07:21 -0700

    Fixed testing

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ from torch.utils.data import DataLoader
 
 from mini_imagenet import MiniImageNet
 from samplers import CategoriesSampler
-from convnet import Convnet
+from models.convnet import Convnet
+from models.resnet import ResNet_baseline
 from utils import pprint, set_gpu, count_acc, Averager, euclidean_metric
 
 
@@ -15,7 +16,7 @@ if __name__ == '__main__':
     parser.add_argument('--load', default='./save/proto-1/max-acc.pth')
     parser.add_argument('--batch', type=int, default=2000)
     parser.add_argument('--way', type=int, default=5)
-    parser.add_argument('--shot', type=int, default=1)
+    parser.add_argument('--shot', type=int, default=5)
     parser.add_argument('--query', type=int, default=30)
     args = parser.parse_args()
     pprint(vars(args))
@@ -26,9 +27,9 @@ if __name__ == '__main__':
     sampler = CategoriesSampler(dataset.label,
                                 args.batch, args.way, args.shot + args.query)
     loader = DataLoader(dataset, batch_sampler=sampler,
-                        num_workers=8, pin_memory=True)
+                        num_workers=8, pin_memory=False)
 
-    model = Convnet().cuda()
+    model = ResNet_baseline().cuda()
     model.load_state_dict(torch.load(args.load))
     model.eval()
 
@@ -51,6 +52,6 @@ if __name__ == '__main__':
         acc = count_acc(logits, label)
         ave_acc.add(acc)
         print('batch {}: {:.2f}({:.2f})'.format(i, ave_acc.item() * 100, acc * 100))
-        
+
         x = None; p = None; logits = None
 
