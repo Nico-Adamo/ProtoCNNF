bd309d76f4fc2c1fec3c26ef86a85d591007e005
Nico-Adamo <nicktwoa@gmail.com>
2021-07-15 12:22:35 -0700

    Updated experiments

--- experiments/train_pre_resnet_21.sh
+++ experiments/train_pre_resnet_21.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python3 pretrain.py --max-epoch 100 \
+python3 pretrain.py --max-epoch 150 \
                  --save-epoch 20 \
                  --query 15 \
-                 --lr 0.002 \
-                 --save-path "models-backbone-feedback/proto-feedback-21" \
+                 --lr 0.001 \
+                 --save-path "models-backbone-feedback/proto-feedback-21-3" \
                  --gpu 2 \
                  --model "ResNet12" \
                  --project "CNNF-Prototype-Pretrain" \
@@ -16,10 +16,10 @@ python3 train.py --max-epoch 200 \
                  --save-epoch 20 \
                  --shot 5 \
                  --way 5 \
-                 --lr 0.0002 \
+                 --lr 0.0001 \
                  --query 15 \
-                 --restore-from "models-backbone-feedback/proto-feedback-21/max-acc.pth" \
-                 --save-path "models/proto-feedback-21-5shot" \
+                 --restore-from "models-backbone-feedback/proto-feedback-21-3/max-acc.pth" \
+                 --save-path "models/proto-feedback-21-5shot-3" \
                  --gpu 2 \
                  --ind-block 2 \
                  --cycles 1 \
