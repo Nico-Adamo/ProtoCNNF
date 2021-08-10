f7606142fd19f073acd7f65818708da4628518a8
Nico-Adamo <nicktwoa@gmail.com>
2021-08-10 00:24:19 -0700

    Bugfixing

--- models/memory_bank.py
+++ models/memory_bank.py
@@ -19,12 +19,12 @@ class MemoryBank(nn.Module):
 
         self.layer1_rn = nn.Sequential(
                 nn.UpsamplingNearest2d(scale_factor=4),
-                nn.Conv2d(640,320,kernel_size=3,padding=0),
+                nn.Conv2d(640,320,kernel_size=2,padding=0),
                 nn.BatchNorm2d(320, momentum=1, affine=True),
                 nn.ReLU(),
                 nn.MaxPool2d(2))
         self.fc1_rn = nn.Sequential(
-                nn.Linear(320 * 2 * 2, 160),
+                nn.Linear(320 * 1 * 1, 160),
                 nn.BatchNorm1d(160, momentum=1, affine=True),
                 nn.ReLU())
         self.fc2_rn = nn.Linear(160, 1)
