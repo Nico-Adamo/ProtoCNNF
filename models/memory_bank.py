b2abc9d56f2c572d1c35b1bf1dbb3b8c45fd2d0c
Nico-Adamo <nicktwoa@gmail.com>
2021-08-11 14:19:09 -0700

    Weird old embedding version

--- models/memory_bank.py
+++ models/memory_bank.py
@@ -10,11 +10,9 @@ class MemoryBank(nn.Module):
     def __init__(self, size):
         super().__init__()
         self.size = size
-
-        self.memory = {"train": torch.tensor([]).cuda(), "val": torch.tensor([]).cuda(), "eval": torch.tensor([]).cuda(), "debug": torch.tensor([]).cuda()}
-
-        self.augment_size = 16 # "Make everything n-shot"
         # Possible self.bias to add to cosine matrix?
+        self.embedding_memory = {"train": torch.tensor([]).cuda(), "val": torch.tensor([]).cuda(), "eval": torch.tensor([]).cuda(), "debug": torch.tensor([]).cuda()}
+        self.image_memory = {"train": torch.tensor([]).cuda()}
 
         self._debug_count = 0
 
@@ -45,97 +43,30 @@ class MemoryBank(nn.Module):
         out = torch.exp(self.alpha) * out + torch.exp(self.beta)
         return out
 
-    def add_memory(self, data, mode = "train"):
-        memory = self.memory[mode]
+    def add_embedding_memory(self, data, mode = "train"):
+        memory = self.embedding_memory[mode]
         # Add memory to the end of the memory bank
         # data: [batch_size, emb_size]
         if memory.size(0) < self.size:
-            self.memory[mode] = torch.cat((memory, data))
+            self.embedding_memory[mode] = torch.cat((memory, data))
         else:
-            self.memory[mode] = torch.cat((memory[data.shape[0]:], data))
+            self.embedding_memory[mode] = torch.cat((memory[data.shape[0]:], data))
 
-    def get_memory(self, mode = "train"):
-        return self.memory[mode]
+    def add_image_memory(self, data, mode = "train"):
+        memory = self.image_memory[mode]
+        # Add memory to the end of the memory bank
+        # data: [batch_size, emb_size]
+        if memory.size(0) < self.size:
+            self.image_memory[mode] = torch.cat((memory, data))
+        else:
+            self.image_memory[mode] = torch.cat((memory[data.shape[0]:], data))
 
-    def reset(self, mode = "train"):
-        self.memory[mode] = torch.tensor([])
+    def get_embedding_memory(self, mode = "train"):
+        return self.embedding_memory[mode]
+
+    def get_image_memory(self, mode = "train"):
+        return self.image_memory[mode]
 
     def get_length(self, mode = "train"):
         return self.memory[mode].size(0)
 
-    def get_similarity_scores(self, support, memory):
-        """
-        Compute the average cosine similarity matrix between support and memory
-        Inputs:
-            Support: [batch_size, n_shot, n_way, n_dim]
-            Memory: [n_way + n_memory, n_dim]
-        Output: [batch_size, n_way, n_shot + n_memory]
-        """
-        memory_t = memory.permute(0,2,1,3)  # [batch_size, n_way, n_shot + n_memory, n_dim]
-        support_t = support.permute(0,2,1,3) # [batch_size, n_way, n_shot, n_dim]
-        memory_t = F.normalize(memory_t, dim=-1)
-        support_t = F.normalize(support_t, dim=-1)
-        # [batch_size, n_way, n_shot, n_dim] x [batch_size, n_way, n_dim, n_shot + n_memory] -> # [batch_size, n_way, n_shot, n_shot + n_memory]
-        cos_matrix = torch.matmul(support_t, memory_t.permute(0,1,3,2))
-        return cos_matrix.mean(dim=2) # [batch_size, n_way, n_shot + n_memory]
-
-    def compute_prototypes(self, support, memory_encoded, debug_support = None):
-        """
-        Augment the support examples with the memory bank, and compute the prototypes
-           self.memory: [n_memory, n_dim]
-           support: [batch_size, n_shot, n_way, n_dim]
-           return: [batch_size, n_way, n_dim]
-        """
-        memory = memory_encoded
-
-        n_memory, _ = memory.shape
-        batch_size, n_shot, n_way, n_dim = support.shape
-        memory_x = memory.view(batch_size, n_memory, 1, n_dim).expand(-1, -1, n_way, -1)
-        shot_memory = torch.cat([support, memory_x], dim=1) # [batch_size, n_shot + n_memory, n_way, n_dim]
-        sim = self.get_similarity_scores(support, shot_memory) # [batch_size, n_way, n_shot + n_memory]
-
-        # Per-instance temperature
-        # memory_weights = self.instance_scale(memory)
-        # support_weights = self.instance_scale(support.view(batch_size * n_shot * n_way, n_dim)).view(batch_size, n_shot, n_way)
-        # memory_weights_x = memory_weights.view(batch_size, n_memory, 1).expand(-1, -1, n_way) # [batch_size, n_shot + n_memory, n_way]
-        # shot_memory_weights = torch.cat([support_weights, memory_weights_x], dim=1).permute(0,2,1) # [batch_size, n_way, n_shot + n_memory]
-        # sim = sim / shot_memory_weights
-
-        mask_weight = torch.cat([torch.tensor([1]).expand(batch_size, n_way, n_shot), torch.tensor([0.5]).expand(batch_size, n_way, n_memory)], dim=-1).cuda()
-        sim = sim * mask_weight
-
-        # Take average along support examples, i.e. compute similarity between each memory/support example and each support example
-        topk, ind = torch.topk(sim, self.augment_size, dim=-1) # [batch_size, n_way, augment_size]
-        res = Variable(torch.zeros(batch_size, n_way, n_shot + n_memory).cuda())
-        sim = res.scatter(2, ind, topk) # Make all weights but top-k 0
-
-        if debug_support is not None:
-            memory_support = self._debug_memory.view(batch_size, n_memory, 1).expand(-1, -1, n_way)
-            debug_support = debug_support.view(batch_size, n_shot, n_way)
-            support_memory_imgs = torch.cat([debug_support, memory_support], dim=1) # [batch_size, n_shot + n_memory, n_way, 3,84,84]
-            support_t = support_memory_imgs.permute(0,2,1) # [batch_size, n_way, n_shot + n_memory, 3,84,84]
-            topk_support = torch.zeros(n_way, self.augment_size)
-            for i in range(n_way):
-                topk_support[i] = support_t[0][i][ind[0][i]]
-            # print(topk_support)
-            # rand_shot = topk_support.view(n_way * 16, *(topk_support.size()[2:])) # [n_way * 8, 3,84,84]
-            # grid = make_grid(rand_shot, nrow=16)
-            # save_image(grid, "memory_images_1500_" + str(self._debug_count)+".png")
-            self._debug_count += 1
-
-            # Make all weights not in the class 0
-            for i in range(n_way):
-                class_num = topk_support[i][0]
-                for j in range(self.augment_size):
-                    if topk_support[i][j] != class_num:
-                        sim[0][i][ind[0][i][j]] = 0
-
-        # mask_thresh = (sim > 0.75).float()
-        # sim = sim * mask_thresh + 1e-8
-        # mask_weight = torch.cat([torch.tensor([1]).expand(batch_size, n_way, n_shot), torch.tensor([0.01]).expand(batch_size, n_way, n_memory)], dim=-1).cuda()
-        # sim = sim * mask_weight
-
-        sim = sim.permute(0,2,1).unsqueeze(-1) # [batch_size, n_shot + n_memory, n_way, 1]
-        proto = (sim * shot_memory).sum(dim=1) / sim.sum(dim=1) # [batch_size, n_way, n_dim]
-
-        return proto
--- models/protonet.py
+++ models/protonet.py
@@ -5,6 +5,7 @@ import numpy as np
 from argparse import Namespace
 from torch.autograd import Variable
 from models.memory_bank import MemoryBank
+from utils import gather
 
 class ProtoNet(nn.Module):
     def __init__(self, args):
@@ -23,6 +24,8 @@ class ProtoNet(nn.Module):
             raise ValueError('')
 
         self.memory_bank = MemoryBank(args.memory_size)
+        self.augment_size = 16 # "Make everything n-shot"
+
         # self.global_w = nn.Conv2d(in_channels=640, out_channels=64, kernel_size=1, stride=1)
         # nn.init.xavier_uniform_(self.global_w.weight)
 
@@ -31,34 +34,26 @@ class ProtoNet(nn.Module):
         return  (torch.Tensor(np.arange(args.way*args.shot)).long().view(1, args.shot, args.way),
                     torch.Tensor(np.arange(args.way*args.shot, args.way * (args.shot + args.query))).long().view(1, args.query, args.way))
 
-    def forward(self, x, memory_bank = False, get_feature = False, validation = False, debug_labels = None):
+    def forward(self, x, memory_bank = False, get_feature = False, mode = "train", debug_labels = None):
         if get_feature:
             return self.encoder(x)
         else:
-            if self.training:
-                memory_mode = "train"
-            elif validation:
-                memory_mode = "val"
-            else:
-                memory_mode = "eval"
             # feature extraction
             x = x.squeeze(0)
             instance_embs = self.encoder(x) # If inter cycle: [cycles + 1, 6 - ind_block, n_batch, n_emb]
                                                                                       # 6: [Pixel space, block 1, 2, 3, 4, pool/flatten][ind_block::]
-            memory_bank = True if self.memory_bank.get_length(mode=memory_mode) > 100 and memory_bank else False
-            # encode memory bank
-            memory_encoded = self.encoder(self.memory_bank.get_memory(mode = memory_mode)) if memory_bank else None
-            # memory_encoded = self.memory_bank.get_memory(mode = memory_mode)
-            # split support query set for few-shot data
-            support_idx, query_idx = self.split_instances(x)
+            memory_bank = True if self.memory_bank.get_length(mode=mode) > 100 and memory_bank else False
+
+            support_idx, query_idx = self.s     plit_instances(x)
             debug_support = x[support_idx.flatten()].view(1, self.args.shot, self.args.way, 3, 84, 84)
             support = instance_embs[support_idx.flatten()].view(*(support_idx.shape + (-1,)))
             query = instance_embs[query_idx.flatten()].view(  *(query_idx.shape   + (-1,)))
 
-            logits = self._forward(support, query, memory_bank = memory_bank, memory_encoded = memory_encoded, debug_support = debug_labels) # add debug_support = debug_support to visualize memory bank
+            logits = self._forward(support, query, memory_bank = memory_bank, mode = mode)
 
             # Update memory bank:
-            self.memory_bank.add_memory(debug_support.view(self.args.way * self.args.shot,3,84,84), mode = memory_mode)
+            self.memory_bank.add_embedding_memory(logits.view(self.args.way * self.args.shot, 640), mode = mode)
+            self.memory_bank.add_image_memory(debug_support.view(self.args.way * self.args.shot,3,84,84), mode = mode)
 
             if debug_labels is not None:
                 self.memory_bank.add_memory(debug_labels, mode = "debug")
@@ -69,15 +64,12 @@ class ProtoNet(nn.Module):
             else:
                 return logits
 
-    def _forward(self, support, query, memory_bank = False, memory_encoded = None, debug_support = None):
+    def _forward(self, support, query, memory_bank = False, mode = "train"):
         emb_dim = support.size(-1)
         # organize support/query data
 
         batch_size, n_shot, n_way, n_dim = support.shape
-        if memory_bank:
-            proto = self.memory_bank.compute_prototypes(support, memory_encoded, debug_support = debug_support)
-        else:
-            proto = support.mean(dim=1)
+        proto = self.compute_prototypes(support, memory_bank = False, mode = mode)
 
         num_batch = proto.shape[0]
         num_proto = proto.shape[1]
@@ -108,3 +100,54 @@ class ProtoNet(nn.Module):
             # logits_support = logits.view(-1, num_proto)
 
         return logits
+
+    def get_similarity_scores(self, support, memory):
+        """
+        Compute the average cosine similarity matrix between support and memory
+        Inputs:
+            Support: [batch_size, n_shot, n_way, n_dim]
+            Memory: [n_way + n_memory, n_dim]
+        Output: [batch_size, n_way, n_shot + n_memory]
+        """
+        memory_t = memory.permute(0,2,1,3)  # [batch_size, n_way, n_shot + n_memory, n_dim]
+        support_t = support.permute(0,2,1,3) # [batch_size, n_way, n_shot, n_dim]
+        memory_t = F.normalize(memory_t, dim=-1)
+        support_t = F.normalize(support_t, dim=-1)
+        # [batch_size, n_way, n_shot, n_dim] x [batch_size, n_way, n_dim, n_shot + n_memory] -> # [batch_size, n_way, n_shot, n_shot + n_memory]
+        cos_matrix = torch.matmul(support_t, memory_t.permute(0,1,3,2))
+        return cos_matrix.mean(dim=2) # [batch_size, n_way, n_shot + n_memory]
+
+    def compute_prototypes(self, support, memory_bank = False, mode="train"):
+        if memory_bank:
+            memory = self.memory_bank.get_embedding_memory(mode=mode)
+            image_memory = self.memory_bank.get_image_memory(mode=mode)
+
+            n_memory, _ = memory.shape
+            batch_size, n_shot, n_way, n_dim = support.shape
+            memory_x = memory.view(batch_size, n_memory, 1, n_dim).expand(-1, -1, n_way, -1)
+            shot_memory = torch.cat([support, memory_x], dim=1) # [batch_size, n_shot + n_memory, n_way, n_dim]
+            sim = self.get_similarity_scores(support, shot_memory) # [batch_size, n_way, n_shot + n_memory]
+
+            mask_weight = torch.cat([torch.tensor([1]).expand(batch_size, n_way, n_shot), torch.tensor([0.2]).expand(batch_size, n_way, n_memory)], dim=-1).cuda()
+            sim = sim * mask_weight
+
+            topk, ind = torch.topk(sim, self.augment_size, dim=-1) # [batch_size, n_way, augment_size]
+            shot_memory_p = shot_memory.permute(0,2,1,3) # [batch_size, n_way, n_shot + n_memory, n_dim]
+            shot_memory_topk = Variable(torch.zeros(batch_size, n_way, self.augment_size, n_dim).cuda())
+            sim_topk = Variable(torch.zeros(batch_size, n_way, self.augment_size).cuda())
+            for way in range(n_way):
+                for shot in range(self.augment_size):
+                    if ind[0][way][shot] < 5 or mode == "eval" or mode == "val": # Support embedding, no need to update
+                        shot_memory_topk[0][way][shot] = shot_memory_p[0][way][ind[0][way][shot]]
+                    else: # Updated embedding
+                        memory_ind = ind[0][way][shot-5]
+                        shot_memory_topk[0][way][shot] = self.encoder(image_memory[memory_ind].unsqueeze(0)).squeeze()
+
+                    sim_topk[0][way][shot] = sim[0][way][ind[0][way][shot]]
+
+            sim = sim.permute(0,2,1).unsqueeze(-1) # [batch_size, augment_size, n_way, 1]
+            shot_memory = shot_memory_topk.permute(0,2,1)  # [batch_size, augment_size, n_way]
+            proto = (sim * shot_memory).sum(dim=1) / sim.sum(dim=1) # [batch_size, n_way, n_dim]
+            return proto
+        else:
+            return support.mean(dim=1)
