30593e857944137493582b2b2a935d507b549393
Nico-Adamo <nicktwoa@gmail.com>
2021-08-14 12:54:05 -0700

    Training script

--- experiments/memory/train.sh
+++ experiments/memory/train.sh
@@ -3,13 +3,15 @@
 python3 train.py --max-epoch 200 \
                  --restore-from "models-backbone-feedback/resnet-21/max-acc-sim.pth" \
                  --no-save \
-                 --gpu 3 \
+                 --gpu 0 \
                  --project "CNNF-Prototype-5shot" \
                  --use-cosine-similarity \
                  --bias-shift \
                  --augment-size 16 \
                  --memory-weight 0.5 \
                  --memory-size 256 \
-                 --test-transduction-steps 10 \
-                 --adaptive-distance
-
+                 --test-memory-size 256 \
+                 --test-memory-weight 0.5 \
+                 --test-query 50 \
+                 --test-augment-size 50 \
+                 --test-transduction-steps 1
