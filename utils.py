4907089e631d6de139972c8d0b4d6b978fd03797
Nico-Adamo <nicktwoa@gmail.com>
2021-10-20 01:59:04 -0700

    Fixing bugs

--- utils.py
+++ utils.py
@@ -177,6 +177,6 @@ def hse_loss(prototypes):
     for i in range(num_proto):
         for j in range(num_proto):
             if i != j:
-                loss += torch.log((prototypes[0][i] - prototypes[0][j]).pow(2).sum().sqrt().pow(-1))
+                loss += torch.log((F.normalize(prototypes[0][i]) - F.normalize(prototypes[0][j])).pow(2).sum().sqrt().pow(-1))
 
     return loss
